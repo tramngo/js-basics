7c6df44e13361f245a74ca04dd1ed0d19f2edb5c
Tram <tramngo2@gmail.com>
2016-09-07 16:13:05 -1000

    completed

--- basics.js
+++ basics.js
@@ -1,24 +1,45 @@
 /* Create a `myName` variable and assign it a String value */
+var myName = 'Tram';
 
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
 
+ var person = {
+ 	name:'Tram',
+ 	age:31
+ };
+
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
+
+ var canDrive = person.age >16;
 
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
 
+ function greet(name) {
+ 	console.log('Hello, my name is '+ name);
+ }
+
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
  */
+
+ var dataTypes = [true, 1, 'good', undefined, null, {}];
 
 /* Create a `dog` object
  * it should have a `bark` function that makes your dog bark!
  * It should also have a name attribute with the value of 'Spot'
  */
+
+var dog = {
+	name: 'Spot',
+	bark: function (name) {
+		console.log(name + 'barks');
+	}
+};
